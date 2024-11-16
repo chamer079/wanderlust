19c16e06caa309d93e2482e5f4c1d4dd924fd4dd
chamer79 <chamer79@gmail.com>
2024-11-15 21:01:21 -0500

    added code to TripList.jsx

--- src/components/TripList/TripList.jsx
+++ src/components/TripList/TripList.jsx
@@ -1,7 +1,4 @@
-
-
-
-
+import { Link } from "react-router-dom";
 
 const TripList = (props) => {
   return (
@@ -9,11 +6,13 @@ const TripList = (props) => {
       <h1>Places I would like to visit...</h1>
       <div>
         {props.trips.map((trip) => (
-          <div key={trip._id}>
-            <img src={trip.image} alt={trip.destination} />
-            <h2>{trip.destination}</h2>
-            {!trip.date ? <h3>TBD</h3> : <h3>{trip.date}</h3>}
-          </div>
+          <Link key={trip._id} to={`/trips/${trip._id}`}>
+            <div>
+              <img src={trip.image} alt={trip.destination} />
+              <h2>{trip.destination}</h2>
+              {!trip.date ? <h3>TBD</h3> : <h3>{trip.date}</h3>}
+            </div>
+          </Link>
         ))}
       </div>
     </>
