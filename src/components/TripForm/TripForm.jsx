cd77e88fd4122f9dd92b6cb030441d85ea2dd534
chamer79 <chamer79@gmail.com>
2024-11-19 22:08:29 -0500

    troubleshooting update trip

--- src/components/TripForm/TripForm.jsx
+++ src/components/TripForm/TripForm.jsx
@@ -1,143 +1,144 @@
-import { useState, useEffect } from "react"
-import { useParams} from "react-router-dom"
-import * as tripService from "../../services/tripService"
-
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import * as tripService from "../../services/tripService";
+import "./TripForm.css";
 
 const TripForm = (props) => {
-    const [formData, setFormData] = useState({
-        destination: "",
-        image: "",
-        date: "",
-        duration: "",
-        accomendationBudget: "",
-        shoppingBudget: "",
-        entertainmentBudget: "",
-        emergencyBudget: ""
-        
-    })
-    // console.log(formData)
+  const [formData, setFormData] = useState({
+    destination: "",
+    image: "",
+    date: "",
+    duration: "",
+    accomendationBudget: "",
+    shoppingBudget: "",
+    entertainmentBudget: "",
+    emergencyBudget: "",
+  });
+  // console.log(formData)
 
-    const { tripId } = useParams()
-    // console.log(tripId)
+  const { tripId } = useParams();
+  // console.log(tripId)
 
-    const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value })
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (tripId) {
+      props.handleUpdateTrip(tripId, formData);
+    } else {
+      props.handleAddTrip(formData);
     }
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        if(tripId){
-            props.handleUpdateTrip(tripId, formData)
-        }else {
-            props.handleAddTrip(formData)
-        }
+    // console.log("formData: ", formData)
+    setFormData({
+      destination: "",
+      image: "",
+      date: "",
+      duration: "",
+      accomendationBudget: "",
+      shoppingBudget: "",
+      entertainmentBudget: "",
+      emergencyBudget: "",
+    });
+    // console.log("formData: ", formData)
+  };
 
-        // console.log("formData: ", formData)
-        setFormData({
-            destination: "",
-            image: "",
-            date: "",
-            duration: "",
-            accomendationBudget: "",
-            shoppingBudget: "",
-            entertainmentBudget: "",
-            emergencyBudget: ""
-        })
-        // console.log("formData: ", formData)
-    }
+  useEffect(() => {
+    const fetchTrip = async () => {
+      const tripData = await tripService.show(tripId);
+      setFormData(tripData);
+      console.log("tripData", tripData)
+    };
+    if (tripId) fetchTrip();
+  }, [tripId]);
 
-    useEffect(() => {
-        const fetchTrip = async () => {
-            const tripData = await tripService.show(tripId)
-            setFormData(tripData)
-            // console.log("tripData", tripData)
-        }
-        if(tripId ) fetchTrip()
-    }, [tripId])
+  return (
+    <>
+      <h1>{tripId ? "Update Your Trip" : "Create a New Trip"}</h1>
+      <form onSubmit={handleSubmit}>
+        <section className="destinationImg">
+          <label htmlFor="destination-input">Destination:</label>
+          <input
+            type="text"
+            name="destination"
+            id="destination-input"
+            value={setFormData.destination}
+            onChange={handleChange}
+          />
+          <label htmlFor="image-input">Image:</label>
+          <input
+            type="text"
+            name="image"
+            id="image-input"
+            value={setFormData.image}
+            onChange={handleChange}
+          />
+        </section>
+        <section className="DateBudget">
+          <article className="DateDuration">
+            <label htmlFor="date-input">Date:</label>
+            <input
+              type="text"
+              name="date"
+              id="date-input"
+              value={setFormData.date}
+              onChange={handleChange}
+            />
+            <label htmlFor="duration-input">Duration:</label>
+            <input
+              type="text"
+              name="duration"
+              id="duration-input"
+              value={setFormData.duration}
+              onChange={handleChange}
+            />
+          </article>
+          <article className="budget">
+            <p>Budget:</p>
+            <label htmlFor="accomendationBudget-input">
+              Accomendations Budget:
+            </label>
+            <input
+              type="number"
+              name="accomendationBudget"
+              id="accomendationBudget-input"
+              value={setFormData.accomendationBudget}
+              onChange={handleChange}
+            />
+            <label htmlFor="shoppingBudget-input">Shopping Budget:</label>
+            <input
+              type="number"
+              name="shoppingBudget"
+              id="shoppingBudget-input"
+              value={setFormData.shoppingBudget}
+              onChange={handleChange}
+            />
+            <label htmlFor="entertainmentBudget-input">
+              Entertainment Budget:
+            </label>
+            <input
+              type="number"
+              name="entertainmentBudget"
+              id="entertainmentBudget-input"
+              value={setFormData.entertainmentBudget}
+              onChange={handleChange}
+            />
+            <label htmlFor="emergencyBudget-input">Emergency Budget:</label>
+            <input
+              type="number"
+              name="emergencyBudget"
+              id="emergencyBudget-input"
+              value={setFormData.emergencyBudget}
+              onChange={handleChange}
+            />
+          </article>
+        </section>
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+};
 
-    return(
-        <>
-            <h1>{tripId ? "Update Your Trip" : "Create a New Trip"}</h1>
-            <form onSubmit={handleSubmit}>
-                <section className="destinationImg">
-                    <label htmlFor="destination-input">Destination:</label>
-                    <input 
-                        type="text"
-                        name="destination"
-                        id="destination-input"
-                        value={setFormData.destination}
-                        onChange={handleChange}
-                    
-                    />
-                    <label htmlFor="image-input">Image:</label>
-                    <input 
-                        type="text"
-                        name="image"
-                        id="image-input"
-                        value={setFormData.image}
-                        onChange={handleChange}
-                       
-                    />
-                </section>
-                <section className="DateBudget">
-                    <article className="DateDuration">
-                        <label htmlFor="date-input">Date:</label>
-                        <input 
-                            type="text"
-                            name="date"
-                            id="date-input"
-                            value={setFormData.date}
-                            onChange={handleChange}
-                        />
-                        <label htmlFor="duration-input">Duration:</label>
-                        <input 
-                            type="text"
-                            name="duration"
-                            id="dutation-input"
-                            value={setFormData.duration}
-                            onChange={handleChange}
-                        />
-                    </article>
-                    <article className="budget">
-                        <h3>Budget:</h3>
-                        <label htmlFor="accomendationBudget-input">Accomendations Budget:</label>
-                        <input 
-                            type="number"
-                            name="accomendationBudget"
-                            id="accomendationBudget-input"
-                            value={setFormData.accomendationBudget}
-                            onChange={handleChange}
-                        />
-                        <label htmlFor="shoppingBudget-input">Shopping Budget:</label>
-                        <input 
-                            type="number"
-                            name="shoppingBudget"
-                            id="shoppingbudget-input"
-                            value={setFormData.shoppingBudget}
-                            onChange={handleChange}
-                        />
-                         <label htmlFor="entertainmentBudget-input">Entertainment Budget:</label>
-                        <input 
-                            type="number"
-                            name="entertainmentBudget"
-                            id="entertainmentBudget-input"
-                            value={setFormData.entertainmentBudget}
-                            onChange={handleChange}
-                        />
-                         <label htmlFor="emergencyBudget-input">Emergency Budget:</label>
-                        <input 
-                            type="number"
-                            name="emergencyBudget"
-                            id="emergencyBudget-input"
-                            value={setFormData.emergencyBudget}
-                            onChange={handleChange}
-                        />
-                    </article>
-                </section>
-                <button type="submit">Submit</button>
-            </form>
-        </>
-    )
-}
-
-export default TripForm
+export default TripForm;
