c4f72fdd6b1daa29d2a09a6b30b6abad6f70af20
chamer79 <chamer79@gmail.com>
2024-11-20 21:41:51 -0500

    styling TripList

--- src/components/TripList/TripList.jsx
+++ src/components/TripList/TripList.jsx
@@ -1,27 +1,54 @@
 import { Link } from "react-router-dom";
+import "./TripList.css";
+import HomeButton from "../../../public/images/HomeButton.png";
 
 const TripList = (props) => {
+  const homeButtonStyle = {
+    width: "100px",
+    height: "100px",
+  };
+
+  const cardImgStyle = {
+    width: "225px",
+    height: "302px",
+  };
+
   return (
-    <>
+    <div className="trip-list">
+      <Link to="/trips">
+        <img src={HomeButton} style={homeButtonStyle} />
+      </Link>
       <h1>Places I would like to visit...</h1>
-      <Link to="/trips/new">
-      <article>
-        <h2>+</h2>
-        <h2>Add a New Destination</h2>
-      </article>
+      <Link style={{ textDecoration: "none" }} to="/trips/new">
+        <article className="new-card">
+          <h2>+</h2>
+          <h2>Add a New Destination</h2>
+        </article>
       </Link>
       <>
         {props.trips.map((trip) => (
-          <Link key={trip._id} to={`/trips/${trip._id}`}>
-            <article>
-              <img src={trip.image} alt={trip.destination} />
-              <h2>{trip.destination}</h2>
-              {!trip.date ? <h3>Date: TBD</h3> : <h3>{trip.date}</h3>}
+          <Link
+            key={trip._id}
+            style={{ textDecoration: "none" }}
+            to={`/trips/${trip._id}`}
+          >
+            <article className="trip-card">
+              <div className="card-img">
+                <img
+                  style={cardImgStyle}
+                  src={trip.image}
+                  alt={trip.destination}
+                />
+              </div>
+              <div className="card-content">
+                <h2>{trip.destination}</h2>
+                {!trip.date ? <h3>Date: TBD</h3> : <h3>{trip.date}</h3>}
+              </div>
             </article>
           </Link>
         ))}
       </>
-    </>
+    </div>
   );
 };
 
